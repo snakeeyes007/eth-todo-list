172ac6fdfad748a3d1bfb28d07b40fa38f4a637d
snakeeyes007 <1805809@kiit.ac.in>
2022-03-01 19:04:38 +0530

    Added blockchain connection

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import Web3 from 'web3';
 import './App.css';
-
-function App() {
+import React, {Component} from  'react';
+class App extends Component{
+  componentWillMount(){
+    this.loadBlockchainData()
+  }
+ async loadBlockchainData(){
+   const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
+   const network = await web3.eth.net.getNetworkType()
+   const accounts = await web3.eth.requestAccounts()
+   this.setState({account:accounts[0]})
+   
+ }
+ constructor(props){
+   super(props)
+   this.state = {account:''}
+ }
+  render(){
   return (
-    <div className="App">
+    <div className="container">
      <h1>Hello!</h1>
-      
+      <p>Your account: {this.state.account}</p>
     </div>
   );
 }
-
+}
 export default App;
